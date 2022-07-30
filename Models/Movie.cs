d0570e235a45e4a598117aff59e50b0b76000742
RuiShuo <ruishuogong@gmail.com>
2022-07-30 18:27:48 +0800

    調整Model的觀賞日期與評分格式

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace PrettyWorld.Models
 {
@@ -7,6 +8,9 @@ namespace PrettyWorld.Models
     {
         public int MovieId { get; set; }
         public string MovieName { get; set; } = null!;
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy/MM/dd}")]
         public DateTime WatchDate { get; set; }
         public string? MovieType { get; set; }
         public string? MoviePicture { get; set; }
@@ -14,6 +18,8 @@ namespace PrettyWorld.Models
         public string? Director { get; set; }
         public string? TopCast { get; set; }
         public string? Review { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F1}", ApplyFormatInEditMode = true)]
         public decimal? Rating { get; set; }
         public int? Plot { get; set; }
         public int? Scene { get; set; }
